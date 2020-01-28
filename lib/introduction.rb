d071283cf9da651a58e9b8c0000228b366b305c3
Johnson Kow <jkow95@gmail.com>
2020-01-28 17:17:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,10 +11,10 @@ end
 introduction("Josh")
 
 def introduction_with_language(name,language)
-  puts "Hi my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-introduction_with_language("Josh","React")
+introduction_with_language("Dan","Ember.js")
 
 
 
